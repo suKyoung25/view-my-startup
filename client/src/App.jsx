91943db2feb269506f5a124b97f4e147ae2dab80
red7578 <red7578@naver.com>
2025-03-26 18:05:55 +0900

    0326 InvestmentModal수정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import GlobalStyle from "./styles/GlobalStyle";
 import Routes from "./routes";
 
-import InvestmentModal from "./components/modal/InvestmentModal";
-
 function App() {
   return (
-    <div style={{ background: "#111", minHeight: "100vh", padding: "2rem" }}>
-      {/* 모달 무조건 화면에 표시 */}
-      <InvestmentModal onClose={() => {}} />
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="App">{Routes()}</div>
+    </>
   );
 }
 
