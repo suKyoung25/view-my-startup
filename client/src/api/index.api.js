0210234396b366e3875fc888ff3060c06ea948dc
ellinax <ellina99@naver.com>
2025-03-27 18:45:20 +0900

    25.03.27 18:47 page/invest-status

--- client/src/api/index.api.js
+++ client/src/api/index.api.js
@@ -1,9 +1,22 @@
+// import axios from "axios";
+
+// const baseURL = "http://localhost:7777";
+// export const client = axios.create({ baseURL });
+
+// const api = {
+//   company,
+//   investment,
+// };
+
+// src/api/index.api.js
+
 import axios from "axios";
 
 const baseURL = "http://localhost:7777";
-export const client = axios.create({ baseURL });
 
-const api = {
-  company,
-  investment,
-};
+export const client = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
