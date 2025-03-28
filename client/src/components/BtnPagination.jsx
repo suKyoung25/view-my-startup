db301d73efb4c9f47a48ebc6756f0620c001c384
ellinax <ellina99@naver.com>
2025-03-28 10:55:25 +0900

    25.03.28 10:55 edit components/BtnPage,BtnPagination

--- client/src/components/BtnPagination.jsx
+++ client/src/components/BtnPagination.jsx
@@ -16,23 +16,37 @@ function BtnPagination({
 
   return (
     <Container>
-      <div onClick={() => currentPage > 1 && onPageChange(currentPage - 1)}>
+      <NavButton
+        onClick={() => currentPage > 1 && onPageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+        aria-label="Previous page"
+      >
         <BtnLeft size={size} />
-      </div>
+      </NavButton>
 
       {pageNumbers.map((num) => (
-        <div key={num} onClick={() => onPageChange(num)}>
-          <BtnPage text={num} size={size} isActive={currentPage === num} />
-        </div>
+        <PageButton
+          key={num}
+          onClick={() => onPageChange(num)}
+          aria-current={currentPage === num ? "page" : undefined}
+        >
+          <BtnPage
+            text={num}
+            size={size}
+            isActive={currentPage === num} // 여기에 주목
+          />
+        </PageButton>
       ))}
 
-      <div
+      <NavButton
         onClick={() =>
           currentPage < totalPages && onPageChange(currentPage + 1)
         }
+        disabled={currentPage === totalPages}
+        aria-label="Next page"
       >
         <BtnRight size={size} />
-      </div>
+      </NavButton>
     </Container>
   );
 }
@@ -41,6 +55,24 @@ const Container = styled.div`
   display: flex;
   flex-direction: row;
   gap: 8px;
+`;
+
+const NavButton = styled.button`
+  background: none;
+  border: none;
+  padding: 0;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 0.5;
+  }
+`;
+
+const PageButton = styled(NavButton)`
+  /* 추가 스타일이 있다면 여기에 */
 `;
 
 export default BtnPagination;
