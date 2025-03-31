5c7ab0b0e99a721e77d7e59af63b903a62f70e9f
ellinax <ellina99@naver.com>
2025-03-31 10:23:03 +0900

    settings server

--- client/src/api/index.api.js
+++ client/src/api/index.api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:7777";
+// const baseURL = "http://localhost:7777";
+const baseURL = "https://fs-d0af.onrender.com";
 
 export const client = axios.create({
   baseURL,
