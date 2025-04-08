c7acfd6a47ecd7e31801b07719d311be9452268d
Shinmilli <126315926+Shinmilli@users.noreply.github.com>
2025-04-08 14:53:30 +0900

    Revert "[신수민] update-detail env&css&node_modules"

--- client/src/api/index.api.js
+++ client/src/api/index.api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 // const baseURL = "http://localhost:7777";
-//const baseURL = "https://fs-d0af.onrender.com";
-const baseURL = import.meta.env.VITE_API_BASE_URL;
+const baseURL = "https://fs-d0af.onrender.com";
 
 export const client = axios.create({
   baseURL,
